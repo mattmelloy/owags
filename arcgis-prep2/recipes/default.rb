2b87711b342c85f6716dafe71f88e1b7387d19b1
mattmelloy <matt@melloy.com.au>
2017-06-04 15:56:41 +1000

    1

--- arcgis-prep2/recipes/default.rb
+++ arcgis-prep2/recipes/default.rb
@@ -1,9 +1,13 @@
+
+directory 'C:/ArcGIS' do
+  action :create
+end
+
 powershell_script 'download_files' do
 	code <<-EOH
-	$bucket = "esriau-231320508276"
-	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_datastore_105.zip" -LocalFile "c:\ArcGIS\arcgis_datastore_105.zip"
-	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_portal_105.zip" -LocalFile "c:\ArcGIS\arcgis_portal_105.zip"
-	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_server_105.zip" -LocalFile "c:\ArcGIS\arcgis_server_105.zip"
-	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_webadaptor_105.zip" -LocalFile "c:\ArcGIS\arcgis_webadaptor_105.zip"
+	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_datastore_105.zip" -LocalFile "c:\ArcGIS\arcgis_datastore_105.zip"
+	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_portal_105.zip" -LocalFile "c:\ArcGIS\arcgis_portal_105.zip"
+	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_server_105.zip" -LocalFile "c:\ArcGIS\arcgis_server_105.zip"
+	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_webadaptor_105.zip" -LocalFile "c:\ArcGIS\arcgis_webadaptor_105.zip"
 	EOH
 end
