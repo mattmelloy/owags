a72e70d7551e95f0d878376cfcccab9a8bf82c0f
mattmelloy <matt@melloy.com.au>
2017-06-04 20:21:08 +1000

    Add windows mod

--- arcgis-prep2/recipes/default.rb
+++ arcgis-prep2/recipes/default.rb
@@ -1,3 +1,4 @@
+include_recipe 'windows'
 
 directory 'C:/ArcGIS' do
   action :create
@@ -11,6 +12,7 @@ powershell_script 'download_files' do
 	if (!(Test-Path "c:/ArcGIS/arcgis_webadaptor_105.zip")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_webadaptor_105.zip" -LocalFile "c:/ArcGIS/arcgis_webadaptor_105.zip" }
 	if (!(Test-Path "c:/ArcGIS/arcgisserver105.ecp")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/ARCGIS/hs.ecp" -LocalFile "c:/ArcGIS/arcgisserver105.ecp" }
 	if (!(Test-Path "c:/ArcGIS/WildcardEsriAuOnline14to19.pfx")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/PORTAL/WildcardEsriAuOnline14to19.pfx" -LocalFile "c:/ArcGIS/WildcardEsriAuOnline14to19.pfx" }
+	not_if {::File.exists?('c:/ArcGIS/arcgis_server_105.zip')}
 	EOH
 end
 
@@ -19,15 +21,9 @@ directory 'C:/ArcGIS/10.5/server' do
   action :create
 end
 
-powershell_script 'Unzip_files' do
-	code <<-EOH
-	$spath = "c:/ArcGIS/arcgis_server_105.zip"
-	$destination = "C:/ArcGIS/10.5/server"
-	Get-ChildItem $spath  -Recurse  | foreach-object  {
-		$archiveFile = $_.fullname | out-string -stream
-		$shellApplication = new-object -com shell.application
-		$zipPackage = $shellApplication.NameSpace($archiveFile)
-		$destinationFolder = $shellApplication.NameSpace($destination)
-		$destinationFolder.CopyHere($zipPackage.Items(), 16) }
-		EOH
+windows_zipfile 'C:/ArcGIS/10.5/server' do
+  source "c:/ArcGIS/arcgis_server_105.zip"
+  action :unzip
+  not_if {::File.exists?('C:/ArcGIS/10.5/server/arcgis_server_105/setup.msi')}
 end
+
