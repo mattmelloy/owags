9e5a23302bdaeba225b54d08c77a57c5461b863f
mattmelloy <matt@melloy.com.au>
2017-06-04 15:44:29 +1000

    Update download script

--- arcgis-prep2/recipes/default.rb
+++ arcgis-prep2/recipes/default.rb
@@ -1,3 +1,9 @@
-powershell_script 'run remote script' do
-    code '. C:\admin\download_arcgis.ps1'
+powershell_script 'download_files' do
+	code <<-EOH
+	$bucket = "esriau-231320508276"
+	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_datastore_105.zip" -LocalFile "c:\ArcGIS\arcgis_datastore_105.zip"
+	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_portal_105.zip" -LocalFile "c:\ArcGIS\arcgis_portal_105.zip"
+	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_server_105.zip" -LocalFile "c:\ArcGIS\arcgis_server_105.zip"
+	Copy-S3Object -BucketName $bucket -Key "SETUP/105/arcgis_webadaptor_105.zip" -LocalFile "c:\ArcGIS\arcgis_webadaptor_105.zip"
+	EOH
 end
