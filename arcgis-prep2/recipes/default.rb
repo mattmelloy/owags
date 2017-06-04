40d37f7020533bd779ac32ad18c5ec49a2ebf8fd
mattmelloy <matt@melloy.com.au>
2017-06-04 16:32:35 +1000

    2

--- arcgis-prep2/recipes/default.rb
+++ arcgis-prep2/recipes/default.rb
@@ -5,16 +5,19 @@ end
 
 powershell_script 'download_files' do
 	code <<-EOH
-	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_datastore_105.zip" -LocalFile "c:/ArcGIS/arcgis_datastore_105.zip"
-	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_portal_105.zip" -LocalFile "c:/ArcGIS/arcgis_portal_105.zip"
-	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_server_105.zip" -LocalFile "c:/ArcGIS/arcgis_server_105.zip"
-	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_webadaptor_105.zip" -LocalFile "c:/ArcGIS/arcgis_webadaptor_105.zip"
-	Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/ARCGIS/hs.ecp" -LocalFile "c:/ArcGIS/arcgisserver105.ecp"
-	Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/PORTAL/WildcardEsriAuOnline14to19.pfx" -LocalFile "c:/ArcGIS/WildcardEsriAuOnline14to19.pfx"
+	if (!(Test-Path "c:/ArcGIS/arcgis_datastore_105.zip")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_datastore_105.zip" -LocalFile "c:/ArcGIS/arcgis_datastore_105.zip" }
+	if (!(Test-Path "c:/ArcGIS/arcgis_portal_105.zip")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_portal_105.zip" -LocalFile "c:/ArcGIS/arcgis_portal_105.zip" }
+	if (!(Test-Path "c:/ArcGIS/arcgis_server_105.zip")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_server_105.zip" -LocalFile "c:/ArcGIS/arcgis_server_105.zip" }
+	if (!(Test-Path "c:/ArcGIS/arcgis_webadaptor_105.zip")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_webadaptor_105.zip" -LocalFile "c:/ArcGIS/arcgis_webadaptor_105.zip" }
+	if (!(Test-Path "c:/ArcGIS/arcgisserver105.ecp")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/ARCGIS/hs.ecp" -LocalFile "c:/ArcGIS/arcgisserver105.ecp" }
+	if (!(Test-Path "c:/ArcGIS/WildcardEsriAuOnline14to19.pfx")) { Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/PORTAL/WildcardEsriAuOnline14to19.pfx" -LocalFile "c:/ArcGIS/WildcardEsriAuOnline14to19.pfx" }
 	EOH
-	not_if (Test-Path "c:/ArcGIS/arcgis_server_105.zip")
 end
 
+
+directory 'C:/ArcGIS/10.5/server' do
+  action :create
+end
 
 windows_zipfile 'C:/ArcGIS/10.5/server' do
   source "c:/ArcGIS/arcgis_server_105.zip"
