cc96ff9d9bcc251b0ba736b45280f3578a387591
mattmelloy <matt@melloy.com.au>
2017-06-04 16:12:48 +1000

    Add unzip

--- arcgis-prep2/recipes/default.rb
+++ arcgis-prep2/recipes/default.rb
@@ -9,5 +9,13 @@ powershell_script 'download_files' do
 	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_portal_105.zip" -LocalFile "c:/ArcGIS/arcgis_portal_105.zip"
 	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_server_105.zip" -LocalFile "c:/ArcGIS/arcgis_server_105.zip"
 	Copy-S3Object -BucketName "esriau-231320508276" -Key "SETUP/105/arcgis_webadaptor_105.zip" -LocalFile "c:/ArcGIS/arcgis_webadaptor_105.zip"
+	Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/ARCGIS/hs.ecp" -LocalFile "c:/ArcGIS/arcgisserver105.ecp"
+	Copy-S3Object -BucketName "esriau-231320508276" -Key "FILES/PORTAL/WildcardEsriAuOnline14to19.pfx" -LocalFile "c:/ArcGIS/WildcardEsriAuOnline14to19.pfx"
 	EOH
+end
+
+
+windows_zipfile "C:/ArcGIS/10.5/server" do
+  source "c:/ArcGIS/arcgis_server_105.zip"
+  action :unzip
 end
