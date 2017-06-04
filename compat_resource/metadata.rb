9c5d276bb5f0bf37d60f1d99e4d4305d9e11abe1
mattmelloy <matt@melloy.com.au>
2017-06-04 21:00:56 +1000

    Update compat_resource

--- compat_resource/metadata.rb
+++ compat_resource/metadata.rb
@@ -1,14 +1,12 @@
 name 'compat_resource'
-maintainer 'John Keiser'
-maintainer_email 'jkeiser@chef.io'
+maintainer 'Lamont Granquist'
+maintainer_email 'lamont@chef.io'
 license 'Apache 2.0'
-description 'Chef 12.5 Resources, For Chef 12.1-12.4'
+description 'Backports functionality introduced in the latest chef-client releases to any chef-client from 12.1 onwards'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version_rb = IO.read(File.expand_path("../files/lib/compat_resource/version.rb", __FILE__))
-raise "Version file not in correct format" unless version_rb =~ /VERSION\s*=\s*'([^']+)'/
-version $1
+version '12.19.0'
 
-chef_version '>= 12.0' if respond_to?(:chef_version)
-source_url 'https://github.com/chef-cookbooks/compat_resource' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/compat_resource/issues' if respond_to?(:issues_url)
+chef_version '>= 12.1' if respond_to?(:chef_version)
+source_url 'https://github.com/chef-cookbooks/compat_resource'
+issues_url 'https://github.com/chef-cookbooks/compat_resource/issues'
